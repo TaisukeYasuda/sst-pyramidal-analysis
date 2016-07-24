cbaf4488aaecb98c19797a2ff8483f4faacc2ab2
taisukeyasuda <tai.taisuke.yasuda@gmail.com>
2016-07-24 13:20:36 -0400

    testing json read

---
+++ js/modules/mainCtrl.js
@@ -0,0 +1,21 @@
+mainApp.controller('mainCtrl', function($scope, $http) {
+    $scope.firstName = "Taisuke";
+    $scope.lastName = "Yasuda";
+
+    $http.get("data/json/cells.json")
+    .then(function(response) {
+      $scope.cells = response.data;
+    },  function(response) {
+      //Second function handles error
+      $scope.firstName = response.data;
+    });
+
+    $http.get("../../data/json/test.txt")
+    .then (function(response) {
+      $scope.firstName = response.data;
+      $scope.lastName = response.data;
+    }, function(response) {
+      //Second function handles error
+      $scope.lastName = response.status;
+    });
+});
